9fd6271d4d9a8ae0bd3a884c2352196d2311c0dd
JerryLeandro <jerryleandro41@gmail.com>
2021-11-26 07:58:19 -0300

    commit inicial

---
+++ src/test/java/com/lilo/boot/LiloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lilo.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
